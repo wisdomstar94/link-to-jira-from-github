a5274740866e22349e1406fb6b685b5738f2bde2
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-27 16:11:06 +0900

    v1.0.4 : pr 상세 페이지에서 reviewrs 에 변화가 있을 시 jira ticket 에 걸려있던 링크가 사라지는 현상 수정.

--- public/content-scripts/github-script.ts
+++ public/content-scripts/github-script.ts
@@ -1,16 +1,21 @@
 import { ISchema } from "../../src/indexeddb/schema.interface";
-import { DomObserver } from '../../src/utils/dom-observer/dom-observer.class';
+import { DomObserver } from "../../src/utils/dom-observer/dom-observer.class";
 
-const domObserver = new DomObserver('head', {
+const domObserver = new DomObserver("head", {
   childList: true,
   subtree: true,
 });
 
-(async() => {
-  chrome.runtime.sendMessage({ requestUrl: '/all_list' }, (response) => {
+const domObserver2 = new DomObserver("#js-global-screen-reader-notice", {
+  childList: true,
+  subtree: true,
+});
+
+(async () => {
+  chrome.runtime.sendMessage({ requestUrl: "/all_list" }, (response) => {
     const items: ISchema.Data.GithubJiraSetupList[] = response.data;
     const fullUrl = window.location.href;
-    const targetItem = items.find(x => fullUrl.startsWith(x.githubBaseUrl));
+    const targetItem = items.find((x) => fullUrl.startsWith(x.githubBaseUrl));
     if (targetItem === undefined) return;
     checkPrDetailPage(targetItem);
     checkCommitDetailPage(targetItem);
@@ -19,32 +24,45 @@ const domObserver = new DomObserver('head', {
 
 function checkPrDetailPage(targetItem: ISchema.Data.GithubJiraSetupList) {
   domObserver.setCallback((mutations) => {
-    dispose();
+    setTimeout(() => {
+      dispose();
+    }, 200);
+  });
+
+  domObserver2.setCallback((mutations) => {
+    setTimeout(() => {
+      dispose();
+    }, 200);
   });
 
   function dispose() {
-    const titleElement = document.querySelector('.gh-header-title');
+    const titleElement = document.querySelector(".gh-header-title");
     // console.log('@titleElement', titleElement);
 
     // titleElements.forEach((titleElement) => {
     if (titleElement === null) return;
-    const is_ltjfg_changed = titleElement.getAttribute('data-is-ltjfg-changed');
-    if (is_ltjfg_changed === 'true') {
+    const is_ltjfg_changed = titleElement.getAttribute("data-is-ltjfg-changed");
+    if (is_ltjfg_changed === "true") {
       // console.log('already applied');
       return;
     }
 
     const html = titleElement.innerHTML;
-    
+
     const regex = /\[.*?\]/;
     const matchedTicketStrings = html.match(regex) ?? [];
     const matchedTicketString = matchedTicketStrings[0];
     if (matchedTicketString === undefined) return;
 
-    const onlyTicketCode = matchedTicketString.replace('[', '').replace(']', '');
-    const newHTML = html.replace(matchedTicketString, `<a class="jira-ticket-a" href="${targetItem.jiraBaseUrl}/browse/${onlyTicketCode}" target="_blank">${matchedTicketString}</a>`);
+    const onlyTicketCode = matchedTicketString
+      .replace("[", "")
+      .replace("]", "");
+    const newHTML = html.replace(
+      matchedTicketString,
+      `<a class="jira-ticket-a" href="${targetItem.jiraBaseUrl}/browse/${onlyTicketCode}" target="_blank">${matchedTicketString}</a>`
+    );
     titleElement.innerHTML = newHTML;
-    titleElement.setAttribute('data-is-ltjfg-changed', 'true');
+    titleElement.setAttribute("data-is-ltjfg-changed", "true");
     // });
   }
   dispose();
@@ -56,26 +74,31 @@ function checkCommitDetailPage(targetItem: ISchema.Data.GithubJiraSetupList) {
   });
 
   function dispose() {
-    const titleElement = document.querySelector('.commit-title');
+    const titleElement = document.querySelector(".commit-title");
     if (titleElement === null) return;
-    const is_ltjfg_changed = titleElement.getAttribute('data-is-ltjfg-changed');
-    if (is_ltjfg_changed === 'true') {
+    const is_ltjfg_changed = titleElement.getAttribute("data-is-ltjfg-changed");
+    if (is_ltjfg_changed === "true") {
       // console.log('already applied');
       return;
     }
-    
+
     const html = titleElement.innerHTML;
-    
+
     const regex = /\[.*?\]/;
     const matchedTicketStrings = html.match(regex) ?? [];
     const matchedTicketString = matchedTicketStrings[0];
     if (matchedTicketString === undefined) return;
 
-    const onlyTicketCode = matchedTicketString!.replace('[', '').replace(']', '');
-    const newHTML = html.replace(matchedTicketString!, `<a class="jira-ticket-a" href="${targetItem.jiraBaseUrl}/browse/${onlyTicketCode}" target="_blank">${matchedTicketString}</a>`);
+    const onlyTicketCode = matchedTicketString!
+      .replace("[", "")
+      .replace("]", "");
+    const newHTML = html.replace(
+      matchedTicketString!,
+      `<a class="jira-ticket-a" href="${targetItem.jiraBaseUrl}/browse/${onlyTicketCode}" target="_blank">${matchedTicketString}</a>`
+    );
     titleElement!.innerHTML = newHTML;
-    titleElement.setAttribute('data-is-ltjfg-changed', 'true');
+    titleElement.setAttribute("data-is-ltjfg-changed", "true");
   }
-  
+
   dispose();
 }
